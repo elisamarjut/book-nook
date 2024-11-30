5ee168423783fd177762399f23be4d2d37efe8ad
elisamarjut <117983133+elisamarjut@users.noreply.github.com>
2024-11-30 21:57:28 +0200

    feature: HomeScreen ImageBackground
    Adds ImageBackround to the HomeScreen. Also changes the position of the welcome text.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
 
     return (
-        <View style={styles.container}>
-            <Text>Welcome to Book Nook!</Text>
-        </View>
+        <ImageBackground
+            style={styles.background}
+            source={require("../bookshelf.jpg")}
+            resizeMode='cover'>
+            <View style={styles.container}>
+                <Text style={styles.text}> Welcome to Book Nook!</Text>
+            </View>
+        </ImageBackground>
+
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginTop: 5
+    },
+    background: {
+        flex: 1,
         justifyContent: 'center',
     },
+    text: {
+        fontWeight: 'bold',
+        fontSize: 30,
+        color: 'brown',
+    }
 });
