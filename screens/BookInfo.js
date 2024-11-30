4aa4b428fe3157befcd09111d73bc153267f510c
elisamarjut <117983133+elisamarjut@users.noreply.github.com>
2024-11-30 23:12:46 +0200

    refactor: BookInfo
    Refactors the styles to gather them all to a stylesheet and call them from there. Adds ScrollView so that even books with long descriptions are displayed nicely. Adds book thumbnail with conditional rendering to show placeholder text if a thumbnail is not available.

--- screens/BookInfo.js
+++ screens/BookInfo.js
@@ -1,14 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function BookInfo({ route }) {
     const { book } = route.params;
 
     return (
-        <View style={styles.container}>
-            <Text style={{ fontWeight: 'bold', fontSize: 20 }}>{book.title}</Text>
-            {book.authors && <Text>Authors: {book.authors.join(', ')}</Text>}
-            {book.description && <Text>Description: {book.description}</Text>}
-        </View>
+        <ScrollView>
+            <View style={styles.container}>
+                {book.imageLinks?.thumbnail ? (
+                    <Image source={{ uri: book.imageLinks?.thumbnail }}
+                        style={styles.image}
+                    />
+                ) : (
+                    <Text style={styles.text}>No picture available</Text>
+                )}
+                <Text style={[styles.text, styles.title]}>{book.title}</Text>
+                {book.authors && <Text style={styles.text}>Authors: {book.authors.join(', ')}</Text>}
+                {book.description && <Text style={styles.text}>Description: {book.description}</Text>}
+            </View>
+        </ScrollView>
     );
 }
 
@@ -19,5 +28,19 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         padding: 20,
+    },
+    image: {
+        width: 150,
+        height: 200,
+        marginBottom: 20,
+    },
+    text: {
+        padding: 10,
+        textAlign: 'justify',
+        lineHeight: 24,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 20,
     },
 });
