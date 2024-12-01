53f80db162b8a650b84171ff60f94aec42c32102
elisamarjut <117983133+elisamarjut@users.noreply.github.com>
2024-12-01 12:21:55 +0200

    refactor: firebaseConfig

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBCh1oYedj7eBEfqoJJA3cAJI_POmhVhLo",
-    authDomain: "book-nook-b8bba.firebaseapp.com",
-    databaseURL: "https://book-nook-b8bba-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "book-nook-b8bba",
-    storageBucket: "book-nook-b8bba.firebasestorage.app",
-    messagingSenderId: "941840806723",
-    appId: "1:941840806723:web:b41a2ae6b44c40b37f75f6"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER,
+    appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
